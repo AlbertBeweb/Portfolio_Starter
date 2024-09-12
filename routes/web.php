d5d43154fe7dbb6bcbd74caf5c52c4c1254b16a5
AlbertBeweb <contact@albert-nicolas.fr>
2024-09-12 11:18:36 +0200

    Create Route

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,26 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/resume', function () {
+    return view('resume');
+});
+
+Route::get('/portfolio', function () {
+    return view('portfolio');
+});
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
