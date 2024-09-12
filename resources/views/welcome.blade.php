e5e586d0ad3732d96f498f74bb58b87af4883bdf
AlbertBeweb <contact@albert-nicolas.fr>
2024-09-12 12:31:17 +0200

    Add script & style welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,11 +10,14 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- Styles -->
     <style>
-        
+        {!! Vite::content('resources/css/app.css') !!}
     </style>
+    <script>
+        {!! Vite::content('resources/js/app.js') !!}
+    </script>
 </head>
 
 <body class="font-sans antialiased dark:bg-black dark:text-white/50">
--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,9 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
+                'resources/css/tailwind.css',
                 'resources/js/app.js',
+                'resources/js/bootstrap.js',
             ],
             refresh: true,
         }),
